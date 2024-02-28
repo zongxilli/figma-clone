a60bed191d1b537e9f9ee3328fdb9827f6b615a9
zongxi2014 <zongxi2014@gmail.com>
2024-02-28 00:05:07 -0500

    add functionality to show other users reactions

--- components/live.tsx
+++ components/live.tsx
@@ -1,18 +1,74 @@
 import { PointerEvent, useCallback, useEffect, useState } from 'react';
-import { useMyPresence, useOthers } from '@/liveblocks.config';
+import {
+	useBroadcastEvent,
+	useEventListener,
+	useMyPresence,
+	useOthers,
+} from '@/liveblocks.config';
 import LiveCursors from './cursor/liveCursors';
 import CursorChat from './cursor/cursorChat';
-import { CursorMode, CursorState, Reaction } from '@/types/type';
+import { CursorMode, CursorState, Reaction, ReactionEvent } from '@/types/type';
 import ReactionSelector from './reaction/reactionButton';
+import FlyingReaction from './reaction/flyingReaction';
+import useInterval from '@/hooks/useInterval';
 
 const Live = () => {
 	const others = useOthers();
+	const broadcast = useBroadcastEvent();
 	const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
 	const [cursorState, setCursorState] = useState<CursorState>({
 		mode: CursorMode.Hidden,
 	});
 	const [reactions, setReactions] = useState<Reaction[]>([]);
+
+	// Remove reactions that are not visible anymore (every 1 sec)
+	useInterval(() => {
+		setReactions((reactions) =>
+			reactions.filter(
+				(reaction) => reaction.timestamp > Date.now() - 4000
+			)
+		);
+	}, 1000);
+
+	useInterval(() => {
+		if (
+			cursorState.mode === CursorMode.Reaction &&
+			cursorState.isPressed &&
+			cursor
+		) {
+			// concat all the reactions created on mouse click
+			setReactions((reactions) =>
+				reactions.concat([
+					{
+						point: { x: cursor.x, y: cursor.y },
+						value: cursorState.reaction,
+						timestamp: Date.now(),
+					},
+				])
+			);
+
+			// Broadcast the reaction to other users
+			broadcast({
+				x: cursor.x,
+				y: cursor.y,
+				value: cursorState.reaction,
+			});
+		}
+	}, 100);
+
+	useEventListener((eventData) => {
+		const event = eventData.event as ReactionEvent;
+		setReactions((reactions) =>
+			reactions.concat([
+				{
+					point: { x: event.x, y: event.y },
+					value: event.value,
+					timestamp: Date.now(),
+				},
+			])
+		);
+	});
 
 	const handlePointerMove = useCallback(
 		(event: PointerEvent) => {
@@ -117,6 +173,16 @@ const Live = () => {
 			className='h-[100vh] w-full flex justify-center items-center text-center'
 		>
 			<h1 className='text-2xl text-white'>Liveblocks Figma Clone</h1>
+
+			{reactions.map((item) => (
+				<FlyingReaction
+					key={item.timestamp.toString()}
+					value={item.value}
+					x={item.point.x}
+					y={item.point.y}
+					timestamp={item.timestamp}
+				/>
+			))}
 
 			{cursor && (
 				<CursorChat
